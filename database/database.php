e85a8a05ffd7d8453ca6f0ccb812283b49eee35c
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-05-17 16:12:20 +0200

    fix all stuff + style on the card

--- database/database.php
+++ database/database.php
@@ -4,8 +4,10 @@ include __DIR__ . '/../Models/Product.php';
 include __DIR__ . '/../Models/ProductAnimal.php';
 
 $products = [
-    new Product('Collare', '8', 'https://www.delashop.it/wp-content/uploads/2021/05/XSblu-picc.jpg'),
-    new ProductAnimal('Lettiera', '18', 'https://www.delashop.it/wp-content/uploads/2021/05/XSblu-picc.jpg', 'cat'),
-    new ProductAnimal('Pallina', '1,99', 'https://www.delashop.it/wp-content/uploads/2021/05/XSblu-picc.jpg', 'dog'),
-    new ProductAnimal('Crocchette', '8', 'https://www.delashop.it/wp-content/uploads/2021/05/XSblu-picc.jpg', 'dog'),
+    new Product('Collare', '8,99', 'https://www.delashop.it/wp-content/uploads/2021/05/XSblu-picc.jpg'),
+    new ProductAnimal('Lettiera', '18,00', 'https://m.media-amazon.com/images/I/61WvnveGuEL._AC_SY355_.jpg', 'cat'),
+    new ProductAnimal('Pallina', '1,99', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ47LYdfU6uVkHUx7iCFLsSnY7eYq2YsuQ5tA&usqp=CAU', 'dog'),
+    new ProductAnimal('Crocchette per cani', '28,00', 'https://www.gardenbedettishop.com/14348-large_default/crocchette-per-cani-prolife-sensitive-grain-free-sensitive-manzo-e-patate-adult-mediumlarge-nutrigenomic-10-kg.jpg', 'dog'),
+    new ProductAnimal('Crocchette per gatti', '17,50', 'https://www.ideashoppingcenter.it/files/archivio_Files/Foto/18893_2.JPG', 'cat'),
+
 ];
--- index.php
+++ index.php
@@ -9,24 +9,26 @@ include __DIR__ . '/database/database.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title> PHP OOP2 </title>
 </head>
 
 <body>
     <div class="container">
         <div class="row">
+
             <?php foreach ($products as $product) : ?>
-                <div class="col">
+                <div class="col-3 p-2">
                     <div class="card">
                         <img src=<?php echo "'$product->image '" ?> alt="">
                         <div class="card-body">
-                            <p>Name <?php echo $product->name ?></p>
-                            <p>Prezzo: <?php echo $product->price ?></p>
-                            <?php if (!is_null($product->type)) : ?>
+                            <p>Nome prodotto : <?php echo $product->name ?></p>
+                            <p>Prezzo: <?php echo $product->price ?>€</p>
+                            <?php if (property_exists($product, 'type')) : ?>
                                 <?php if ($product->type == 'cat') : ?>
-                                    <i class="fa-solid fa-cat fs-3 p-2 rounded-circle bg-info"></i>
+                                    <p>Adatto per: </p> <i class="fa-solid fa-cat fs-3 p-2 rounded-circle bg-info"></i>
                                 <?php elseif ($product->type == 'dog') : ?>
-                                    <i class="fa-solid fa-dog fs-3 p-2 rounded-circle bg-info"></i>
+                                    <p>Adatto per: </p> <i class="fa-solid fa-dog fs-3 p-2 rounded-circle bg-success"></i>
                                 <?php endif;  ?>
                             <?php endif;  ?>
                         </div>
